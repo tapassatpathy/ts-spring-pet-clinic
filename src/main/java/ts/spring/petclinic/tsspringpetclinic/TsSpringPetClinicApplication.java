b19df1d68762788750cbe43f5a4798089e53a74a
tapas.satpathy <tapas.satpathy@gmail.com>
2018-11-15 23:29:36 +0900

    Pet-clinic basic project add

---
+++ src/main/java/ts/spring/petclinic/tsspringpetclinic/TsSpringPetClinicApplication.java
@@ -0,0 +1,12 @@
+package ts.spring.petclinic.tsspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsSpringPetClinicApplication.class, args);
+	}
+}

17c5b112c8e282fba4f8bc592eed275b42b49be9
tapas.satpathy <tapas.satpathy@gmail.com>
2018-11-17 10:43:09 +0900

    Created the modules and model entities. Closes #1

--- ts-spring-petclinic-web/src/main/java/com/tapas/basicSpring/spring5project/TsSpringPetClinicApplication.java
+++ ts-spring-petclinic-web/src/main/java/com/tapas/basicSpring/spring5project/TsSpringPetClinicApplication.java
@@ -1,4 +1,4 @@
-package ts.spring.petclinic.tsspringpetclinic;
+package com.tapas.basicSpring.spring5project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ts-spring-petclinic-web/src/test/java/com/tapas/basicSpring/spring5project/TsSpringPetClinicApplicationTests.java
+++ ts-spring-petclinic-web/src/test/java/com/tapas/basicSpring/spring5project/TsSpringPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package ts.spring.petclinic.tsspringpetclinic;
+package com.tapas.basicSpring.spring5project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TsSpringPetClinicApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
